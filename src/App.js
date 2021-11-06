842a30f0015828d049dcacad30e7bbf2c28c2a36
sahhebali <saheb.co21@gmail.com>
2021-11-06 19:54:37 +0530

    react-core-concepts2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import reactDom from 'react-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserPost></UserPost>
+
     </div>
   );
 }
 
+function UserPost() {
+  const [user, setUserPost] = useState([]);
+  useEffect(() => {
+    fetch('http://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setUserPost(data))
+  }, [])
+  return (
+    <div>
+      <div>External Post</div>
+      {
+        user.map(user => <Users title={user.title}></Users>)
+      }
+
+    </div>
+  )
+}
+
+function Users(props) {
+  return (
+    <div>
+      <p>title :{props.title}</p>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default App;
+/* amar pracktice */
+/* function ExternalUser() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUser(data))
+  }, [])
+  return (
+    <div>
+      <h1>External:</h1>
+      {
+        user.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+function User(props) {
+  return (
+    <div style={{ backgroundColor: 'salmon', color: 'black', border: '3px solid blue', margin: '10px' }}>
+      <h3>Name : {props.name}</h3>
+      <p>email : {props.email}</p>
+    </div>
+  )
+} */
+
+/* ----------------------------------------- */
+
+/* function ExternalUser() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUser(data))
+  }, [])
+
+  return (
+    <div>
+      <h3>External User</h3>
+      {
+        user.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div >
+      <h3>Name :{props.name} </h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+} */
+
+
+
+
+/* men vdo dekhe pracktice  */
+/* function Counter() {
+  const [count, setCount] = useState(5);
+  const handleIncries = () => setCount(count + 1);
+  const handleDecries = () => setCount(count - 1);
+
+
+          return (
+          <div>
+            <h1>Count : {count}</h1>
+            <button onClick={handleDecries}>decries</button>
+            <button onClick={handleIncries}>Incries</button>
+
+          </div>
+          )
+} */
+
+
+
+
+/* function Counter() {
+  const [count, setCount] = useState(5);
+  const handleIncries = () => setCount(count + 1);
+  const handleDcries = () => setCount(count - 1);
+
+
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={handleDcries}>Decries</button>
+      <button onClick={handleIncries}>Incries</button>
+    </div>
+  )
+} */
